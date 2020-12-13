57a3ed44b763d2f006e5176d53a1de3a03f32824
ssamilg <samil-98@hotmail.com>
2020-12-13 23:48:12 +0300

    navigation to recipe details updated

--- app/features/home/components/LatestRecipes.js
+++ app/features/home/components/LatestRecipes.js
@@ -2,19 +2,27 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Card, useTheme, Button } from 'react-native-paper';
 import { paddings, margins } from '~/config/styles';
+import { useDispatch } from 'react-redux';
+import { refreshRecipe } from '~/features/main/redux/actions';
 
 const LatestRecipes = (props) => {
+  const dispatch = useDispatch();
   const { py1, pa4, px2, py3 } = paddings;
-  const { ma2, my0, mt4, mt6, mx1 } = margins;
+  const { mx4, my2, mt4, mt6, mx1 } = margins;
   const { colors } = useTheme();
+
+  const navigateToRecipeDetails = (recipe) => {
+    dispatch(refreshRecipe(recipe));
+    props.navigation.navigate('RecipeDetails', {
+      recipe,
+    });
+  };
 
     const latestRecipeList = props.latestRecipes.map(recipe => 
       <View key={recipe.recipeTitle}>
         <Card
-          style={[pa4, ma2]}
-          onPress={() => { props.navigation.navigate('RecipeDetails', {
-            recipe,
-          }) }}
+          style={[pa4, mx4, my2]}
+          onPress={() => navigateToRecipeDetails(recipe)}
         >
           <Card.Cover
             style={{ height: 100, width: '100%', resizeMode: 'contain' }}
