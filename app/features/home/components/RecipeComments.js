d96a6915fe35b08df5b4cf5d1e0356d72c642d0c
ssamilg <samil-98@hotmail.com>
2020-12-13 23:48:51 +0300

    comment bugs fixed

--- app/features/home/components/RecipeComments.js
+++ app/features/home/components/RecipeComments.js
@@ -1,28 +1,14 @@
-import { func } from 'prop-types';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Card, useTheme, Title, Caption } from 'react-native-paper';
 import { paddings, margins } from '~/config/styles';
+import { useSelector, useDispatch } from 'react-redux';
 
 const RecipeComments = ({ recipe }) => {
   const { py2, pt4, px4, py3 } = paddings;
   const { mt2, mt4, ml4, mx4, my1, mx1 } = margins;
   const { colors } = useTheme();
-  const [recipeComments, setRecipeComments] = React.useState([]);
-  getRecipeComments();
-
-  async function getRecipeComments() {
-    if (recipe.comments) {
-      setRecipeComments(recipe.comments);
-    } else {
-      setRecipeComments([{
-        id: 0,
-        user: { email: '' },
-        commentText: 'Henüz hiç yorum yok...' ,
-        dateCreated: '',
-      }]);
-    }
-  };
+  const recipeComments = useSelector(state => state.mainReducer.recipe.comments); 
 
     const comments = recipeComments.map((comment) => 
       <View key={comment.id}>
