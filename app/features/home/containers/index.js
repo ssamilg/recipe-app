b09c9868630755f507783e4812db46af80078d95
ssamilg <samil-98@hotmail.com>
2020-12-19 21:22:32 +0300

    slider removed

--- app/features/home/containers/index.js
+++ app/features/home/containers/index.js
@@ -13,19 +13,13 @@ import { paddings, margins } from '~/config/styles';
 import CommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import firestore from '@react-native-firebase/firestore';
 
-const windowWidth = Dimensions.get('window').width;
-
 export default function Home({ navigation }) {
   const dispatch = useDispatch();
   const { colors } = useTheme();
   const { py1, pt2, pa2 } = paddings;
-  const { ma1, ma2, my0, mx2, mt6, mx1, my4 } = margins;
+  const { ma1, ma2, my0, mx2, mt6, mx1, my4, ml1 } = margins;
   const [loading, setLoading] = useState(false);
-  const carouselRef = useRef(null);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [entries, setEntries] = React.useState([]);
   const [latestRecipes, setLatestRecipe] = React.useState([]);
-  const [todaysRecipe, setTodaysRecipe] = React.useState([]);
   fetchLatestRecipes();
 
   const onLogout = () => {
@@ -38,27 +32,6 @@ export default function Home({ navigation }) {
       });
   };
 
-  const RenderItem = ({ item }) => {
-    return (
-      <>
-        <View style={[ma1, { borderRadius: 15, overflow: 'hidden' }]}>
-          <Image
-            style={{ height: 200, width: '100%', resizeMode: 'stretch' }}
-            source={{
-              uri: item.photoLink,
-            }}
-          />
-        </View>
-
-        <View style={[ma1, { flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
-          <Text style={[ material.title ]}>
-            { item.recipeTitle }
-          </Text>
-        </View>
-      </>
-    );
-  };
- 
   async function fetchLatestRecipes() {
     firestore()
       .collection('Recipes')
@@ -79,40 +52,15 @@ export default function Home({ navigation }) {
         });
 
         setLatestRecipe(tempArray);
-        // const slider = tempArray.map((item) => {
-        //   const newItem = {
-        //     title: item.recipeTitle,
-        //     photo: item.photoLink,
-        //   }
-
-        //   return newItem;
-        // });
-
-        // setEntries(slider);
+        
       })
       .finally(() => {
-        fetchTodaysRecipe();
+        // fetchTodaysRecipe();
       });
+      
+
   };
 
-  async function fetchTodaysRecipe() {
-    firestore()
-      .collection('Recipes')
-      .limit(1)
-      .orderBy('likes', 'desc')
-      .get()
-      .then((querySnapshot ) => {
-        querySnapshot.forEach(documentSnapshot => {
-          const recipe = {
-            id: documentSnapshot.id,
-            ...documentSnapshot.data(),
-          }
-
-          setTodaysRecipe(recipe);
-        });
-      })
-  };
-  
   const props = {
     latestRecipes,
     navigation,
@@ -120,38 +68,32 @@ export default function Home({ navigation }) {
 
   return (
     <ScrollView>
-      <LinearGradient colors={['#BDBDBD', '#FFF']} style={[pt2]}>
-        <Carousel
-          ref={carouselRef}
-          data={latestRecipes}
-          renderItem={RenderItem}
-          sliderWidth={windowWidth}
-          itemWidth={windowWidth - 64}
-          onSnapToItem={index => setActiveIndex(index)}
-        />
-        <Pagination
-          dotsLength={latestRecipes.length}
-          activeDotIndex={activeIndex}
-          containerStyle={[py1, my0]}
-          dotStyle={{
-            width: 8,
-            height: 8,
-            borderRadius: 4,
-            marginHorizontal: 4,
-            backgroundColor: '#212121',
-          }}
-          inactiveDotOpacity={0.4}
-          inactiveDotScale={0.6}
-        />
-      </LinearGradient>
-
-      <View style={[mt6]}>
+      <View>
         <Card style={[pa2, ma2, { borderWidth: 2, borderColor: '#F48FB1', borderStyle: 'solid' } ]}>
-          <Card.Title title="Günün Tarifi" subtitle={todaysRecipe.recipeTitle}/>
-          <Card.Cover
+          <Card.Title title="Günün Menüsü" subtitle="Afiyet Olsun..."/>
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <CommunityIcon color="black" name="label" size={20}/>
+              <Text style={[ml1]}>lul</Text>
+            </View>
+
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <CommunityIcon color="black" name="label" size={20}/>
+              <Text style={[ml1]}>lel</Text>
+            </View>
+
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <CommunityIcon color="black" name="label" size={20}/>
+              <Text style={[ml1]}>lol</Text>
+            </View>
+
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <CommunityIcon color="black" name="label" size={20}/>
+              <Text style={[ml1]}>lil</Text>
+            </View>
+          {/* <Card.Cover
             style={{ height: 100, width: '100%', resizeMode: 'contain' }}
             source={{ uri: todaysRecipe.photoLink }}
-          />
+          /> */}
         </Card>
       </View>
 
