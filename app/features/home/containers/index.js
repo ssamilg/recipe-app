0962c724bdf98a14fa11d4bc450d95634ec819ac
ssamilg <samil-98@hotmail.com>
2020-12-24 00:57:44 +0300

    comments added

--- app/features/home/containers/index.js
+++ app/features/home/containers/index.js
@@ -1,3 +1,4 @@
+// Bu dosyada kullanilacak elementler import edildi 
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { Button, Card, useTheme } from 'react-native-paper';
@@ -11,11 +12,13 @@ import CommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import firestore from '@react-native-firebase/firestore';
 
 export default function Home({ navigation }) {
+  // Local degiskenler tanimlandi
   const dispatch = useDispatch();
   const { colors } = useTheme();
   const { pa2 } = paddings;
   const { ma2, mx2, mx1, my4, ml1 } = margins;
   const [loading, setLoading] = useState(false);
+  const [isNewDay, setIsNewDay] = useState(false);
   const [latestRecipes, setLatestRecipe] = React.useState([]);
   const [todaysMenu, setTodaysMenu] = React.useState([]);
 
@@ -24,6 +27,7 @@ export default function Home({ navigation }) {
     fetchLatestRecipes();
   }, [fetchLatestRecipes, createTodaysMenu])
 
+  // Logout methodu yazildi
   const onLogout = () => {
     setLoading(true);
     auth()
@@ -34,6 +38,7 @@ export default function Home({ navigation }) {
       });
   };
 
+  // Son tarifleri cektigimiz method yazildi
   const fetchLatestRecipes = useCallback(
     () => {
       firestore()
@@ -61,9 +66,11 @@ export default function Home({ navigation }) {
     [],
   );
 
+  // Gunun menusunu olusturdugumuz method yazildi
   const createTodaysMenu = useCallback(
     () => {
-      if (checkTheDay()) {
+      checkTheDay();
+      if (isNewDay) {
         const todaysMenuTemplate = {
           soup: '',
           mainMeal: '',
@@ -101,8 +108,11 @@ export default function Home({ navigation }) {
     [todaysMenu],
   );
   
+  // Gunu kontrol icin method yazildi
   const checkTheDay = useCallback(
     () => {
+      let lastMenu = {};
+
       firestore()
         .collection('Menus')
         .limit(1)
@@ -113,14 +123,16 @@ export default function Home({ navigation }) {
             if (documentSnapshot.data()) {
               setTodaysMenu(documentSnapshot.data());
             }
-
-            return (new Date().getDay() > documentSnapshot.data().date.toDate().getDay());
+            
+            lastMenu = documentSnapshot.data();
           });
+          setIsNewDay(new Date().getDay() > lastMenu.date.toDate().getDay());
         })
     },
     [],
   );
 
+  // Gunun menusun guncellendigi method yazildi
   const updateTodaysMenu = useCallback(
     (todaysMenuTemplate) => {
       firestore()
@@ -142,6 +154,7 @@ export default function Home({ navigation }) {
   };
 
   return (
+    // Anasayfa tasarimi
     <ScrollView>
       <View>
         <Card style={[pa2, ma2, { borderWidth: 2, borderColor: '#F48FB1', borderStyle: 'solid' } ]}>
@@ -203,7 +216,7 @@ export default function Home({ navigation }) {
         icon="logout"
         mode="outlined"
         onPress={onLogout}>
-        Logout
+        Çıkış Yap
       </Button>
     </ScrollView>
   );
