5bb48ad2061bb527be95763887a3949d3b8bf550
ssamilg <samil-98@hotmail.com>
2020-12-24 21:14:06 +0300

    null check added

--- app/features/home/components/LatestRecipes.js
+++ app/features/home/components/LatestRecipes.js
@@ -3,7 +3,7 @@ import React, { useCallback } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-paper';
 import { paddings, margins } from '~/config/styles';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { refreshRecipe } from '~/features/main/redux/actions';
 import CommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import firestore from '@react-native-firebase/firestore';
@@ -15,7 +15,10 @@ const LatestRecipes = (props) => {
   const { pa4 } = paddings;
   const { mx4, my2, mr1 } = margins;
   // Giris yapilan kullanici id'si cekildi
-  const { uid } = auth().currentUser._user;
+  let uid = '';
+  if (auth().currentUser) {
+    uid = auth().currentUser._user.uid;
+  }
 
   // Tarif detayina gitmemizi saglayan navigasyon methodu
   const navigateToRecipeDetails = (recipe) => {
